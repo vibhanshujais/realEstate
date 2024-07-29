e0d71a1ec9908df0337954b1271b6ad695607923
Vibhanshu <vibhanshujaiswal28@gmail.com>
2024-07-29 20:44:40 +0530

    updated

--- realestate/realestate/urls.py
+++ realestate/realestate/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from realestate.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('login/', login),
+    path('register/', register),
 ]
---
+++ realestate/realestate/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
